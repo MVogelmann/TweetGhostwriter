361339fb113c7968030b328dd4945b093f8b5eb9
Unknown <mvogelmann@mail.csuchico.edu>
2017-04-06 12:29:55 -0700

    Random sentences created

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,18 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Our mind ", "Knowledge ", "Our consciousness ", "An incredible fact "];
+  var body = ["is the intelligence of ", "is the basic of ", "is considered the key to ", "is to keep balance between ", "is our future to "];
+  var end = ["our selves ", "happiness"];
+  randIndex = randomUpTo(intro.length - 1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length - 1);
+  sentence += body[randIndex];
+  randIndex = randomUpTo(end.length - 1);
+  sentence += end[randIndex];
+  //console.log(sentence);
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
